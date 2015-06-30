934b5059913855237c0cb29fa01fdaaf5bfdde2c
Derwin McGeary <derwinmcgeary+web@gmail.com>
2015-06-30 19:38:53 +0100

    update AndroidManifest.xml, move setContentView to Question.java, clean MainActivity.java

--- app/src/main/java/com/example/android/vocabbuilder/Question.java
+++ app/src/main/java/com/example/android/vocabbuilder/Question.java
@@ -21,6 +21,7 @@ public class Question extends AppCompatActivity{
     // TODO: A Question could have a method to select from Vocabulary
     // TODO: A Question could create several Answer Objects to use
     // TODO:
+    int correct;
     MediaPlayer correctSound;
 
     @Override
@@ -28,18 +29,19 @@ public class Question extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         correctSound = MediaPlayer.create(this, R.raw.correct);
+        TextView promptText = (TextView) findViewById(R.id.promptText);
         Vocabulary vocab = new Vocabulary();
         Context context = this.getApplicationContext();
         int nAns = 3; // 3 for now, could be 2 or 4 or whatever
         int imRes[] = {R.id.button1, R.id.button2, R.id.button3}; // TODO: Get these programatically
         Random rn = new Random();
-        int correct = rn.nextInt(nAns);
-
+        correct = rn.nextInt(nAns);
         Word Answers[] = vocab.getn(nAns);
-
+        promptText.setText(Answers[correct].getWordText());
         for (int i = 0; i < Answers.length; i++) {
             ImageButton butto = (ImageButton) findViewById(imRes[i]);
             butto.setImageResource(Answers[i].imageRes(context));
+            if(i == correct) {butto.setTag("correct");} else {butto.setTag("wrong");}
         }
     }
     public void checkAnswer(View view) {
@@ -50,7 +52,7 @@ public class Question extends AppCompatActivity{
         // Checks if the correct answer was given or not
         // and gives corresponding result
         // TODO: update if statement so answer is not hard-coded
-        if (answerSubmitted.equals("button1")) {
+        if (answerSubmitted.equals("correct")) {
             correctSound.start();
             feedbackText.setText("Correct");
         } else {
@@ -76,9 +78,5 @@ public class Question extends AppCompatActivity{
 
 
         }
-
-
-/*    Answer[correct].getAudio();
-    Then do some display magic and wait for an answer */
-    }
+}
 
