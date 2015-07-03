2080d3e053dd093995525b1dc2199ef21a4d648d
Derwin McGeary <derwinmcgeary+web@gmail.com>
2015-07-03 12:41:29 +0100

    Zoom in effect in LanguageSelectorActivity

--- app/src/main/java/com/example/android/vocabbuilder/LanguageSelectorActivity.java
+++ app/src/main/java/com/example/android/vocabbuilder/LanguageSelectorActivity.java
@@ -1,12 +1,16 @@
 package com.example.android.vocabbuilder;
 
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 
 import java.util.Locale;
@@ -19,31 +23,48 @@ public class LanguageSelectorActivity extends AppCompatActivity {
         setContentView(R.layout.language_selector_activity);
         Context context;
         context = this.getApplicationContext();
+        Animation zoomIn = AnimationUtils.loadAnimation(this, R.anim.language_select);
+
+
         SharedPreferences settings = getSharedPreferences("PrefsFile", MODE_PRIVATE);
-        String currentLocale = "en"; // initialise it as non-NULL
-        currentLocale = Locale.getDefault().getLanguage().substring(0,2);
+        //noinspection
+        String currentLocale = Locale.getDefault().getLanguage().substring(0, 2);
         String lang = settings.getString("language", currentLocale);
         // Swap so that last used or current locale is at top of the list
-        String locales[]= {"en", "ru", "es", "fr"};
+        String locales[] = {"en", "ru", "es", "fr"};
         int j = 0;
-        for(int i =0; i < locales.length; i++) {
-            if(lang.equalsIgnoreCase(locales[i])) j = i;
+        try {
+            for (int i = 0; i < locales.length; i++) {
+                if (lang.equalsIgnoreCase(locales[i])) j = i;
+            }
+            String temp = locales[0];
+            locales[0] = locales[j];
+            locales[j] = temp;
+        }catch(NullPointerException e) { // just skip it then
         }
-        String temp = locales[0];
-        locales[0] = locales[j];
-        locales[j] = temp;
-
         int imRes[] = {R.id.button1, R.id.button2, R.id.button3, R.id.button4};
         for(int i =0; i < locales.length; i++) {
             ImageButton b = (ImageButton) findViewById(imRes[i]);
             b.setImageResource(context.getResources().getIdentifier(locales[i], "drawable", context.getPackageName()));
             b.setTag(locales[i]);
+            b.startAnimation(zoomIn);
         }
     }
     public void setLanguage(View view){
-        // Get the language from the button tag
         String langRequested = view.getTag().toString();
 
+        /* This was weird and glitchy, so it's commented out
+         int imRes[] = {R.id.button1, R.id.button2, R.id.button3, R.id.button4};
+        Context context = this.getApplicationContext();
+        // Get the language from the button tag
+        // Start animation
+        Animation zoomOut = AnimationUtils.loadAnimation(this, R.anim.language_selected);
+        for(int i =0; i < imRes.length; i++) {
+            ImageButton b = (ImageButton) findViewById(imRes[i]);
+            b.startAnimation(zoomOut);
+        }
+        */
+        // and do some work!
         SharedPreferences settings = getSharedPreferences("PrefsFile", MODE_PRIVATE);
         SharedPreferences.Editor editor = settings.edit();
         editor.putString("language", langRequested);
