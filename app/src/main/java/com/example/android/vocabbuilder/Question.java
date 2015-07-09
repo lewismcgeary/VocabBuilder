4e6ce792070dbf2d60fabcf94c28dc484364a40c
Derwin McGeary <derwinmcgeary+web@gmail.com>
2015-07-09 23:31:55 +0100

    Create Quiz.java, refine Question.java

--- app/src/main/java/com/example/android/vocabbuilder/Question.java
+++ app/src/main/java/com/example/android/vocabbuilder/Question.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.Random;
 
 /**
+ * This class gets one Question.
+ * A Question has 3 Words, a correct Answer, and a boolean[] of which guesses have been made
  * Created by derwin on 09/07/15.
  */
 public class Question {
@@ -26,9 +28,14 @@ public class Question {
         for(int i = 0; i <nOptions; i++) guessed[i] = false;
     }
 
+    public ArrayList<Word> getWords() {
+        return words;
+    }
     public int getAnswer(){
         return Answer;
     }
-
+    public boolean isGuessed(int i){
+        return guessed[i];
+    }
 
 }
---
+++ app/src/main/java/com/example/android/vocabbuilder/Quiz.java
@@ -0,0 +1,49 @@
+package com.example.android.vocabbuilder;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import java.util.ArrayList;
+
+/**
+ * This class maintains a Quiz object for the duration of one quiz,
+ * and returns Questions on demand.
+ * Created by derwin on 09/07/15.
+ */
+public class Quiz {
+
+private Context myCtx;
+ArrayList<Question> quiz;
+    int nQuestions;
+    int currentQuestion;
+
+    public Quiz(Context context, Vocabulary vocab){
+        ArrayList<Question> myQs = new ArrayList<>();
+        this.myCtx = context;
+        Resources res = myCtx.getResources();
+        nQuestions = res.getInteger(R.integer.numberOfQuestions);
+        for(int i = 0; i < nQuestions; i++){
+            Question addme = new Question(myCtx,vocab);
+            myQs.add(addme);
+        }
+        quiz = myQs;
+        currentQuestion = 0;
+    }
+
+    public Question getQuestion(int n){
+        return quiz.get(n);
+    }
+
+    public Question getCurrentQuestion(){
+        return quiz.get(currentQuestion);
+    }
+
+    public boolean incrementQuestionNumber(){
+        currentQuestion++;
+        return currentQuestion < nQuestions; // If false, we've overshot!
+    }
+
+    public int getQuestionNumber(){
+        return currentQuestion;
+    }
+}
