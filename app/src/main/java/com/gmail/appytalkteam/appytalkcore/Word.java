26fdddb5aeab97163a1cd6e5683a08c2804a93fc
Lewis McGeary <lewismcgeary@gmail.com>
2016-05-18 01:05:46 +0100

    clean up Word class

--- app/src/main/java/com/gmail/appytalkteam/appytalkcore/Word.java
+++ app/src/main/java/com/gmail/appytalkteam/appytalkcore/Word.java
@@ -7,55 +7,11 @@ import android.os.Parcelable;
  * Created by Lewis on 22/06/15.
  */
 public class Word implements Parcelable{
-    /**
-     *Creates an object containing the text of a word and the locations of corresponding image &
-     * audio files
-    */
-    public static final Parcelable.Creator<Word> CREATOR = new Parcelable.Creator<Word>() {
 
-        public Word createFromParcel(Parcel in) {
-            return new Word(in);
-        }
-
-
-        public Word[] newArray(int size) {
-            return new Word[size];
-        }
-    };
-
-    public String wordText;
-    public int imageResourceId;
-    public int audioResourceId;
-    public String category="None";
-
-    public void setWordText(String wordText) {
-        this.wordText = wordText;
-    }
-
-    public void setImageResourceId(int imageResourceId) {
-        this.imageResourceId = imageResourceId;
-    }
-
-    public void setAudioResourceId(int audioResourceId) {
-        this.audioResourceId = audioResourceId;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-    //
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(imageResourceId);
-        dest.writeString(wordText);
-        dest.writeInt(audioResourceId);
-        dest.writeString(category);
-    }
-
-    // default constructor for no particular reason other than stopping errors
-    public Word(){
-        // empty!
-    }
+    private String wordText;
+    private int imageResourceId;
+    private int audioResourceId;
+    private String category="None";
 
     public Word(int imageResId, String word, int audioResId){
         imageResourceId =imageResId;
@@ -70,6 +26,35 @@ public class Word implements Parcelable{
         category = Category;
     }
 
+    public int getImageResourceId(){
+        return imageResourceId;
+    }
+    public int getAudioResourceId(){
+        return audioResourceId;
+    }
+    public String getWordText(){
+        return wordText;
+    }
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     *Creates an object containing the text of a word and the locations of corresponding image &
+     * audio files
+     */
+    public static final Parcelable.Creator<Word> CREATOR = new Parcelable.Creator<Word>() {
+
+        public Word createFromParcel(Parcel in) {
+            return new Word(in);
+        }
+
+
+        public Word[] newArray(int size) {
+            return new Word[size];
+        }
+    };
+
     private Word(Parcel in){
         imageResourceId = in.readInt();
         wordText = in.readString();
@@ -77,17 +62,13 @@ public class Word implements Parcelable{
         category = in.readString();
     }
 
-    public int getImageResourceId(){
-        return imageResourceId;
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(imageResourceId);
+        dest.writeString(wordText);
+        dest.writeInt(audioResourceId);
+        dest.writeString(category);
     }
-    public int getAudioResourceId(){
-        return audioResourceId;
-    }
-    String getWordText(){
-        return wordText;
-    }
-    String getCategory() { return category;}
-
 
     @Override
     public int describeContents() {
